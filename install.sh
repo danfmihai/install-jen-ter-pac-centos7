2204e761ba03bc9f475e7c3581f4694bca286c27
mihai <misu3108@yahoo.com>
2021-09-29 18:34:42 +0000

    edited

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 clear
 current_user=$(whoami)
+ip=$(ip a | grep eth0 | grep 'inet' | awk '{ print $2}' | head --bytes=15)
 
 echo "Installing jenkins on Centos"
-
+echo
+sleep 2
 sudo yum install -y wget curl git unzip
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -15,7 +17,10 @@ echo
 echo "Jenkins Installation done!"
 echo
 sleep 2
+clear
 echo "Installing Packer"
+echo
+sleep 2
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install packer
@@ -23,8 +28,10 @@ echo
 echo "Packer installation done!"
 packer --version
 sleep 2
+clear
 echo "Instaling Terraform"
 echo
+sleep 2
 wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
 unzip terraform_*
 sudo mv terraform* /bin
@@ -37,17 +44,18 @@ sleep 2
 # testing if user jenkins exists
 echo "Adding users to sudoers:"
 if getent passwd jenkins > /dev/null 2>&1; then
-    echo 'jenkins ALL=(ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
+    echo 'jenkins ALL=(ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo > /dev/null 2>&1
 else
-    echo "No, the user does not exist"
+    echo "No, the user jenkins does not exist"
 fi
-echo "$current_user ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR="tee -a" visudo
-
+echo "$current_user ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR="tee -a" visudo > /dev/null 2>&1
+echo "Done!"
 echo "alias jen='sudo su - jenkins -s /bin/bash'" >> ~/.bashrc
-source ~/.bashrc
 echo
-echo "Installed jenkins,packer,terraform!"
+echo "Installed Jenkins, Packer, Terraform!"
 echo
 echo "To login as jenkins user type: jen"
-echo
+echo "To get the admin password to setup Jenkins got to:  http://${ip}:8080 and follow the instructions."
+sudo cat  /var/lib/jenkins/
+source ~/.bashrc
 sleep 2
